bbc0b118052a772617fb142244cb1ce73af16d06
miilll <milaandriani0642@gmail.com>
2023-01-14 15:04:22 +0700

    CRUD_PenjualanBarang

---
+++ resources/views/layout/nav.blade.php
@@ -0,0 +1,33 @@
+<!-- ======= Sidebar ======= -->
+<aside id="sidebar" class="sidebar">
+
+    <ul class="sidebar-nav" id="sidebar-nav">
+
+      <li class="nav-item">
+        <a class="nav-link " href="{{url('form_user')}}">
+          <i class="bi bi-incognito"></i>
+          <span>User</span>
+        </a>
+      </li>
+      <!-- End User Nav -->
+      
+      <li class="nav-item">
+        <a class="nav-link " href="{{url('form_barang')}}">
+          <i class="bi bi-cart4"></i>
+          <span>Barang</span>
+        </a>
+      </li>
+      <!-- End Barang Nav -->
+
+      <li class="nav-item">
+        <a class="nav-link " href="{{url('form_member')}}">
+          <i class="bi bi-pin-angle-fill"></i>
+          <span>Members</span>
+        </a>
+      </li>
+      <!-- End Member Nav -->
+
+    </ul>
+
+</aside>
+<!-- End Sidebar-->
